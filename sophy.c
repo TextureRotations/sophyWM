f335a1239b02b21a78c96eceb91d1fac0f1aa3b8
ii <egorfimoz@gmail.com>
2025-08-13 23:09:33 +0200

    keypress event

--- sophy.c
+++ sophy.c
@@ -1,5 +1,6 @@
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
+#include <X11/XKBlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -7,43 +8,49 @@ static Display *dpy;
 static Window root;
 
 typedef struct Arg {
-	char **v;
+    char **v;
 } Arg;
 
 typedef struct KeyEvent {
-	int modifier;
-	int key;
-	void (*func)(Arg *a); // todo: replace func with more reasonable name 
-	Arg arg;
+    int modifier;
+    int key;
+    void (*func)(Arg *a);
+    Arg arg;
 } KeyEvent;
-
-#include "config.h"
 
 void spawn(Arg *a);
 void keypress(XEvent *event);
 
+#include "config.h"
+
 void spawn(Arg *a) {
-	fprintf(stderr, "haiii :P\n");
+    fprintf(stderr, "haiii :P\n");
 }
 
-void keypress(XEvent *event) {
-
+void keypress(XEvent *e) {
+    KeySym keysym = XkbKeycodeToKeysym(dpy, e->xkey.keycode, 0, 0);
+    for (unsigned int i = 0; i < sizeof(keys)/sizeof(*keys); ++i)
+        if (keys[i].key == keysym &&
+        	keys[i].modifier == e->xkey.state)
+            keys[i].func(&keys[i].arg);
 }
 
-static void (*eventhandler[]) (XEvent *event) = {
-	[KeyPress] = keypress, 
+static void (*eventhandler[])(XEvent *event) = {
+    [KeyPress] = keypress,
 };
 
 int main(void) {
-	if (!(dpy = XOpenDisplay(NULL))) {
-		fprintf(stderr, "error opening display\n");
-		return 1;
-	}
-	root = DefaultRootWindow(dpy);
-	
-	XEvent event;
-	while (1 && XNextEvent(dpy, &event)) { // loop blocks until something happens
-		if 	(eventhandler[event.type]) 
-			 eventhandler[event.type] (&event);
-	}
+    if (!(dpy = XOpenDisplay(NULL))) {
+        fprintf(stderr, "error opening display\n");
+        return 1;
+    }
+    root = DefaultRootWindow(dpy);
+    XSelectInput(dpy, root, KeyPressMask);
+
+    XEvent event;
+    while (!XNextEvent(dpy, &event)) {
+        if (eventhandler[event.type])
+            eventhandler[event.type](&event);
+    }
 }
+
